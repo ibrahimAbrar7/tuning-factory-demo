007c810dbbffaf47c847c50bd42e383cee5dd59f
Mohd Ibrahim Ahmed <mohdabrar41111@gmail.com>
2025-02-21 22:19:42 +0000

    yes

--- app/Components/ThemeToggle.tsx
+++ app/Components/ThemeToggle.tsx
@@ -11,7 +11,7 @@ const ThemeToggle = () => {
   return (
     <button
       onClick={() => setTheme(theme === "light" ? "dark" : "light")}
-      className="fixed top-40 right-3 md:right-10 p-2 bg-gray-100 dark:bg-gray-900 dark:border border border-gray-400 dark:border-gray-500 rounded-full hover:bg-gray-300 dark:hover:bg-gray-600 focus:outline-none z-50 animate-bounce duration-1000"
+      className="fixed top-40 right-3 md:right-10 p-2 bg-gray-100 dark:bg-gray-900 dark:border border border-gray-400 dark:border-gray-500 rounded-full hover:bg-gray-300 dark:hover:bg-gray-600 focus:outline-none z-50 animate-bounce duration-3000"
     >
       {theme === "light" ? (
         <MdLightMode className="w-6 h-6 text-yellow-500" />
