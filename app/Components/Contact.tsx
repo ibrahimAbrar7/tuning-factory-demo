6946dac7ee94c282a784bd0f8f18a4424f9843e0
Mohd Ibrahim Ahmed <mohdabrar41111@gmail.com>
2025-02-21 21:02:37 +0000

    Nightout changes

--- app/Components/Contact.tsx
+++ app/Components/Contact.tsx
@@ -4,12 +4,12 @@ const Contact: React.FC = () => {
   return (
     <div className="bg-black dark:bg-stone-950 text-center py-12">
       <h1 
-        className="text-white text-6xl lg:text-7xl font-bold animate-bounce-1s"
+        className="text-white text-5xl lg:text-6xl font-bold animate-bounce-1s font-barlow"
       >
         Contact Us
       </h1>
       <h2 
-        className="text-orange-400 text-4xl lg:text-5xl font-bold mt-4 animate-bounce-1s"
+        className="text-orange-400 text-3xl lg:text-4xl font-bold mt-4 animate-bounce-1s font-barlow"
       >
         CONTACT OUR EXPERT
       </h2>
