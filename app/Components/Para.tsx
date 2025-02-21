1b9bc5da69d4fa4bb9c470738decf6ca3989337b
Mohd Ibrahim Ahmed <mohdabrar41111@gmail.com>
2025-02-21 22:05:54 +0000

    done

--- app/Components/Para.tsx
+++ app/Components/Para.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Para: React.FC = () => {
   return (
-    <div className="bg-white dark:bg-slate-950 py-8 md:py-12 px-12 text-justify md:px-48">
+    <div className="bg-white dark:bg-slate-950 py-8 md:py-12 px-12 md:text-justify md:px-48">
       <p className="text-black text-lg md:text-xl dark:text-white animate-bounce-1s">
         Tuning Factory is a small automotive hobby shop based in Calgary, 
         Alberta .  We cater to specific makes and models from different
